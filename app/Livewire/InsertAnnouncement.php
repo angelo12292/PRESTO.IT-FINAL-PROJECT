8d410b0d67b195a0c467e20b24e4eedb1d24b7a9
angelo alberga <angeloalberga@Air-di-angelo.lan>
2024-03-06 20:37:09 +0100

    validatori

--- app/Livewire/InsertAnnouncement.php
+++ app/Livewire/InsertAnnouncement.php
@@ -6,17 +6,23 @@ use Livewire\Component;
 use App\Models\Category;
 use App\Models\Announcement;
 use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Validate;
 
 
 class InsertAnnouncement extends Component
 {
+    #[Validate('required|max:50')] 
     public $AnnTitle;
     public $AnnCategory;
+    #[Validate('required|max:200')] 
     public $AnnDescrip;
+    #[Validate('required|min:1')] 
     public $AnnPrice;
 
     public function store()
     {
+        $this->validate();
+        
         Announcement::create([
             'title'=> $this->AnnTitle,
             'user_id'=>Auth::id(),
@@ -25,10 +31,13 @@ class InsertAnnouncement extends Component
             'price'=>$this->AnnPrice,
         ]);
 
-    session()->flash('success', 'Annuncio creato con successo!');
+        session()->flash('success', 'Annuncio creato con successo!');
+
+        $this->dispatch('announcements-created');
 
 
     }
+    
     public function render()
     {
         return view('livewire.insert-announcement', ['categories'=>Category::all()]);
