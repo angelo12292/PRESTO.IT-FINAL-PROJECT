42381d60197d419536ad81d004a3490bb23bfa28
Maurizio Angeletti <91mangele@gmail>
2024-03-06 19:31:31 +0100

    creazione componente livewire showannouncements

---
+++ app/Livewire/ShowAnnouncements.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ShowAnnouncements extends Component
+{
+    public $announcements = [];
+
+    public function render()
+    {
+        return view('livewire.show-announcements');
+    }
+
+    public function
+}
