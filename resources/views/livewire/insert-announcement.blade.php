3a0df3018a6f3bf829467102eb068f9f1b3dfcae
angelo alberga <angeloalberga@Air-di-angelo.lan>
2024-03-07 19:45:52 +0100

    funzione reset form

--- resources/views/livewire/insert-announcement.blade.php
+++ resources/views/livewire/insert-announcement.blade.php
@@ -4,34 +4,37 @@
     <form wire:submit.prevent="store">
     
     <x-success />
+    <div class="row">
+        <div class="col-6 m-auto">
+            <div class="mb-3">
+                <label for="name" class="form-label">Titolo</label>
+                <input type="text" class="form-control" id="title" placeholder="nome annuncio" wire:model="AnnTitle">
+                @error('AnnTitle') <span class="text-danger small">{{ $message }}</span> @enderror
+            </div>
+            <div class="mb-3">
+                <label for="price" class="form-label">Prezzo € </label>
+                <input type="number" class="form-control" id="price" placeholder="prezzo" wire:model="AnnPrice">
+                @error('AnnPrice') <span class="text-danger small">{{ $message }}</span> @enderror
+            </div>
+            <div class="mb-3">
+            <select id="category_id" class="form-select" wire:model="AnnCategory">
+                @foreach($categories as $category)
+                <option value="{{$category->id}}">{{$category->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="description" class="form-label">Descrizione</label>
+                <textarea class="form-control" id="description" rows="3" wire:model="AnnDescrip">
 
-         <div class="mb-3">
-            <label for="name" class="form-label">Titolo</label>
-            <input type="text" class="form-control" id="title" placeholder="nome annuncio" wire:model="AnnTitle">
-            @error('AnnTitle') <span class="text-danger small">{{ $message }}</span> @enderror
-        </div>
-        <div class="mb-3">
-            <label for="price" class="form-label">Prezzo € </label>
-            <input type="number" class="form-control" id="price" placeholder="prezzo" wire:model="AnnPrice">
-            @error('AnnPrice') <span class="text-danger small">{{ $message }}</span> @enderror
-        </div>
-        <div class="mb-3">
-        <select id="category_id" class="form-select" wire:model="AnnCategory">
-             @foreach($categories as $category)
-            <option value="{{$category->id}}">{{$category->name}}</option>
-                @endforeach
-            </select>
-        </div>
-        <div class="mb-3">
-            <label for="description" class="form-label">Descrizione</label>
-            <textarea class="form-control" id="description" rows="3" wire:model="AnnDescrip">
-
-            </textarea>
-            @error('AnnDescrip') <span class="text-danger small">{{ $message }}</span> @enderror
-        </div>
-        <div class="mb-3">
-            <button class="btn btn-light" type="submit">Crea</button>
-        </div>
+                </textarea>
+                @error('AnnDescrip') <span class="text-danger small">{{ $message }}</span> @enderror
+            </div>
+            <div class="mb-3">
+                <button class="btn btn-light" type="submit">Crea</button>
+            </div>
+            </div>
+         </div>
     </form>
     </div>
 </div>
