2db12a55fa9ee57c300e48ead1a54cc4833471fc
angelo alberga <angeloalberga@Air-di-angelo.lan>
2024-03-07 19:06:07 +0100

    livewire categorie

---
+++ app/Livewire/SearchByCategories.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class SearchByCategories extends Component
+{
+    public function render()
+    {
+        return view('livewire.search-by-categories');
+    }
+}
