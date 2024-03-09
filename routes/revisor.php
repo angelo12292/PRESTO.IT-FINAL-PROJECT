9f171898e22952e197117d03a0e0c91d950b7627
angelo alberga <angeloalberga@Air-di-angelo.lan>
2024-03-09 21:03:13 +0100

    terzo commit conferma e annulla annunci revisor

--- routes/revisor.php
+++ routes/revisor.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/home', [RevisorController::class, 'index'])->middleware('isRevisor')->name('revisor.index');
+Route::get('/home', [RevisorController::class, 'index'])->middleware('IsRevisor')->name('revisor.index');
 
-Route::patch('/accetta/annuncio/{announcement}',[RevisorController::class, 'acceptAnnouncement'])->middleware('isRevisor')->name('revisor.accept_announcement');
+Route::patch('/accetta/annuncio/{announcement}',[RevisorController::class, 'acceptAnnouncement'])->middleware('IsRevisor')->name('revisor.accept_announcement');
 
-Route::patch('/rifiuta/annuncio/{announcement}',[RevisorController::class, 'rejectAnnouncement'])->middleware('isRevisor')->name('revisor.reject_announcement');
+Route::patch('/rifiuta/annuncio/{announcement}',[RevisorController::class, 'rejectAnnouncement'])->middleware('IsRevisor')->name('revisor.reject_announcement');
