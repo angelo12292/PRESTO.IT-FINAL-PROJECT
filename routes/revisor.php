6fff81e4a460e1b41d3ae11b5efb411be8c5672b
angelo alberga <angeloalberga@Air-di-angelo.lan>
2024-03-09 21:00:32 +0100

    secondo commit di cancellazione e conferma revisor annunci

--- routes/revisor.php
+++ routes/revisor.php
@@ -4,12 +4,9 @@ use App\Http\Controllers\RevisorController;
 use Illuminate\Support\Facades\Route;
 
 
-<<<<<<< HEAD
+
 Route::get('/home', [RevisorController::class, 'index'])->middleware('isRevisor')->name('revisor.index');
 
 Route::patch('/accetta/annuncio/{announcement}',[RevisorController::class, 'acceptAnnouncement'])->middleware('isRevisor')->name('revisor.accept_announcement');
 
 Route::patch('/rifiuta/annuncio/{announcement}',[RevisorController::class, 'rejectAnnouncement'])->middleware('isRevisor')->name('revisor.reject_announcement');
-=======
-Route::get('/home', [RevisorController::class, 'index'])->name('revisor.index');
->>>>>>> 2e90f8c40c2a9905a4f2d764ebe75bf9e8b0ed6c
