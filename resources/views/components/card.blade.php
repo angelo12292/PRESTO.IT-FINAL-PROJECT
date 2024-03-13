882813b2b997f4414cd3709ca0faf3d44ff66410
Fabrizio Malcangi <fmalcangi83@gmail.com>
2024-03-13 18:29:32 +0100

    mod card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,7 +1,10 @@
 <a href="{{ $root }}" class="">
   <div class="card  reveal reveal.active rounded-4 h-100 shadow card-scale border-0 position-relative " style="width: 22rem;">
     <div class="position-absolute z-1 p-2 px-3 bg-white w-75 rounded-5 userBar primary-color-text">
-      <i class="bi bi-person-circle fs-2 "></i>
+      <div class="d-flex align-items-center">
+        <i class="bi bi-person-circle fs-2 "></i>
+        <h6 class="m-0 ms-2 ">{{$user}}</h6>
+      </div>
 
     </div>
     <div class="position-relative ">
@@ -14,7 +17,10 @@
     </div>
 
     <div class="card-body rounded-4 d-flex flex-column gap-1 justify-content-between py-4 pt-5 px-4">
-      <p class="card-text m-0 primary-color-text">{{$category}}</p>
+      <div class="d-flex">
+        <i class="bi bi-tags-fill accent-color-text"></i>
+        <p class="card-text m-0 primary-color-text ms-2 ">{{$category}}</p>
+      </div>
       <h5 class="card-title primary-color-text">{{$title}}</h5>
 
       <!-- <p class="card-text text-truncate"><span class="h6">Descrizione: </span>{{$description}}</p> -->
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -111,7 +111,7 @@
     <div class="row g-2">
       @foreach($announcements as $announcement)
       <div class="col-4 mb-4 d-flex justify-content-center ">
-        <x-card :price="$announcement->price" :description="$announcement->description" :category="$announcement->category->name" :title="$announcement->title" :root="route('announce.View',$announcement->id)" />
+        <x-card :user="$announcement->user->name" :price="$announcement->price" :description="$announcement->description" :category="$announcement->category->name" :title="$announcement->title" :root="route('announce.View',$announcement->id)" />
       </div>
       @endforeach
     </div>
