5d5ee25a60f7c835272ecefb1b99f13cfeaf583d
Mirko.Cibei <mirko.cibei@libero.it>
2024-03-14 20:54:30 +0100

    fake

--- resources/views/livewire/insert-announcement.blade.php
+++ resources/views/livewire/insert-announcement.blade.php
@@ -1,5 +1,5 @@
 <div class="container-fluid p-0 ">
-    <x-nav-home />
+    <x-nav />
     <div class="d-flex flex-column justify-content-between pt-5" style="height: 100vh">
         <div class="col-12 mt-5 pt-5 ">
             <form wire:submit.prevent="store">
