5bbd95b8df0a8063f0cd51c0db313eb8aab7b198
angelo alberga <light_a@hotmail.it>
2024-03-20 19:55:50 +0100

    fake1

--- resources/views/livewire/insert-announcement.blade.php
+++ resources/views/livewire/insert-announcement.blade.php
@@ -43,7 +43,7 @@
                                     <div class="row border border-4 border-info rounded shadow py-4 ">
                                         @foreach($images as $key => $image)
                                         <div class="col my-3">
-                                            <img src="{{$image->temporaryUrl()}}" class=" mx-auto shadow rounded" style="width: 100%;height:100%)">
+                                            <img src="{{$image->temporaryUrl()}}" class=" mx-auto shadow rounded" style="width: 100%;height:100%">
                                             <button type="button" class="btn btn-danger shadow d-block text-center mt-2 mx-auto"
                                             wire:click="removeImage({{$key}})">
                                             Cancella
