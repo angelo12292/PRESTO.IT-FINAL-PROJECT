e60c3141af6a1fddd829bc733097bf7a4eea8cb0
Maurizio Angeletti <91mangele@gmail>
2024-03-28 09:48:59 +0100

    sistemate notifiche nelle navbar

--- resources/views/livewire/notifications-list.blade.php
+++ resources/views/livewire/notifications-list.blade.php
@@ -1,15 +1,25 @@
 <div >
     <h6 class="primary-color-text ps-3 border-top mb-0 py-2">NOTIFICHE</h6>
-    <ul class="list-group ">
-        
-        @foreach($notifications as $notification)
-        
-        <li class="list-group-item ps-3 w-100">
-            <a href="{{route('notification.index')}}" class="nav-link primary-color-text dropDownHover w-100 text-start "
-              type="submit">{{$notification->body}}</a>
-        </li>
-        
-        @endforeach
+    <ul class="list-group">
+        @if(count($notifications))
+            @foreach($notifications as $notification)
+            
+            <li class="list-group-item lh-1 p-0 w-100">
+                <a href="{{route('notification.index')}}" class="fs-6 p-2 small nav-link primary-color-text dropDownHover w-100 text-start "
+                type="submit">{{$notification->body}}</a>
+            </li>
+            <li class="list-group-item bg-danger text-center p-0"> 
+            <button wire:click="clearNotificationList({{ $notification->id }})" type="button" class="small fs-6 btn px-5 btn-danger btn-sm">cancella</button>
+            </li>
+            @endforeach
+            
+                
+            
+        @else
+            <li class="primary-color-text list-group-item ps-3 w-100">
+                <p>non ci sono nuove notifiche</p>
+            </li>
+        @endif    
     </ul>
         
 
